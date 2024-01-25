9ec8c4164cc8ee9914114989881d5ed8b72f711d
Keskyll <k.keskyll@gmail.com>
2024-01-25 10:47:35 +0200

    Objects

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-let fs = require('fs');
-fs.writeFileSync('hello.text', 'hello Node!');
+let obj ={
+    name: 'Karoli',
+    hello(){
+        console.log('hello');
+    }
+}
+
+console.log(obj.name);
+obj.hello();
+
+let text = 'Hello world';
+console.log(text.lenght);
+console.log(text[2]);
+console.log(text.toUpperCase());
+let array = [1, 2, 3];
+array.push(4);
+console.log(console);
+console.log(Math.PI);
+console.log(Math);
