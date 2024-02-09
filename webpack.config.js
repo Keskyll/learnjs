69f4626614de17303fcd7df0f4ef811186d8c67e
Keskyll <k.keskyll@gmail.com>
2024-02-09 10:51:02 +0200

    Add css

--- webpack.config.js
+++ webpack.config.js
@@ -7,16 +7,32 @@ console.log(path.resolve(_dirname, 'dist'));
 
 module.exports = {
   entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
     },
-    compress: true,
-    port: 9000,
-  },
-  plugins: [new HtmlWebpackPlugin()],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+    },
+    module: {
+         rules: [
+            {
+            test: /\.css$/i,
+             use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+  plugins: [
+        new HtmlWebpackPlugin({
+             template: './src/index.html'
+         }),
+        new HtmlWebpackPlugin({
+             template: './src/index.html'
+         }),
+
+    ],
 };
