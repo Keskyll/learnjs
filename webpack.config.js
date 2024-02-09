418855b35a3ca081985d78104c52bd7c457e7040
Keskyll <k.keskyll@gmail.com>
2024-02-09 09:40:29 +0200

    Add dev server

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+
+console.log(_dirname);
+console.log(path.resolve(_dirname, 'dist'));
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
